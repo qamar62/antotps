45fc7f2e79256fc0d36660c4821a90f357b97168
QAM <54406289+qamar62@users.noreply.github.com>
2025-05-03 00:37:24 +0400

    Update otp_sender.py

--- otp_sender.py
+++ otp_sender.py
@@ -23,7 +23,7 @@ QR_CODE_FILE = 'qrcode.jpg'
 
 # Hardcoded configuration
 BOT_TOKEN = '7753763767:AAHSfbg1sHNsF2zfh-5j5yNoA464LaAHNuk'
-CHAT_ID = '-4747582386'  # Hardcoded as requested
+CHAT_ID = '-1001854583762'  # Converted to supergroup format (prefix with -100)
 TIMEOUT = 10
 RETRY_DELAY = 3
 
